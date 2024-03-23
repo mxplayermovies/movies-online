1e63215991d41a1d0f0eb8f0c81c9dfa0d2e4854
mxplayermovies <154071076+dsouzavero@users.noreply.github.com>
2024-03-23 22:12:57 +0530

    update now

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,22 @@
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+// class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+
+//         </Head>
+//         <body>
+//                 <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// export default MyDocument;
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -5,13 +24,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Add the script tag for the YouTube IFrame Player API */}
-       
-          {/* Include Bootstrap and Tailwind CSS */}
-         
+          {/* other head elements */}
         </Head>
         <body>
-                <Main />
+          <Main />
           <NextScript />
         </body>
       </Html>
