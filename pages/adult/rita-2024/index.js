f9313b5f2e5f8e9de4f145dc2b4af049efdf26d6
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-03-30 03:09:17 +0530

    test Back image

--- pages/adult/rita-2024/index.js
+++ pages/adult/rita-2024/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import MovieDetails from '@components/MovieDetails'
 import DailyMotionBackground from '@components/DailyMotionBackground'
 import AdultSkipAds from '@components/AdultSkipAds'
+import ComingSoon from '@components/ComingSoon'
 
 const Index = ({ movieData }) => {
   return (
     <div>
       <AdultSkipAds movieId='INDEX125' />
+      <ComingSoon movieId='INDEX125' />
       <MovieDetails movieId='INDEX125' />
       <DailyMotionBackground movieId='INDEX125' />
     </div>
