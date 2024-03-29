190bb1b2a7c201604b9bf022a452cf328392d278
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-03-30 02:34:04 +0530

    see it

--- pages/adult/eks-2024/index.js
+++ pages/adult/eks-2024/index.js
@@ -1,22 +1,17 @@
 
-
 import React from 'react'
-import MovieDetails from '@components/MovieDetails';
-import DailyMotionBackground from '@components/DailyMotionBackground';
+import MovieDetails from '@components/MovieDetails'
+import DailyMotionBackground from '@components/DailyMotionBackground'
 import AdultSkipAds from '@components/AdultSkipAds'
 
-const Index = () => {
-
-
-
+const index = () => {
   return (
     <div>
-      <MovieDetails movieId='INDEX78' />
-      <AdultSkipAds movieId='INDEX78' />
-      <DailyMotionBackground movieId='INDEX78' />
-    
+     <MovieDetails movieId="INDEX78" />;
+     <AdultSkipAds movieId='INDEX78' />;
+     <DailyMotionBackground movieId='INDEX78' />
     </div>
-  );
-};
+  )
+}
 
-export default Index;
+export default index;
