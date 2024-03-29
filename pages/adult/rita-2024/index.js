d5e9eada321e46c6caa864c2b4d8aa243ba9902b
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-03-30 03:11:45 +0530

    popup

--- pages/adult/rita-2024/index.js
+++ pages/adult/rita-2024/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import MovieDetails from '@components/MovieDetails'
 import DailyMotionBackground from '@components/DailyMotionBackground'
 import AdultSkipAds from '@components/AdultSkipAds'
-import ComingSoon from '@components/ComingSoon'
+import ComingPop from '@components/ComingPop'
 
 const Index = ({ movieData }) => {
   return (
     <div>
       <AdultSkipAds movieId='INDEX125' />
-      <ComingSoon movieId='INDEX125' />
+      <ComingPop movieId='INDEX125' />
       <MovieDetails movieId='INDEX125' />
       <DailyMotionBackground movieId='INDEX125' />
     </div>
