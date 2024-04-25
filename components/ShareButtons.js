0c715e28fdcbcb89dcaeaea2a2de7eab447d4e7b
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-04-25 22:14:32 +0530

    revise update

--- components/ShareButtons.js
+++ components/ShareButtons.js
@@ -12,43 +12,30 @@ import {
   EmailIcon,
 } from 'react-share';
 
-const ShareButtons = ({ image }) => {
-  const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
-
-  // Combine current URL with image URL if available
-  const imageUrlWithCurrentUrl = image ? `${currentUrl} - ${image}` : currentUrl;
+const ShareButtons = ({ title, image }) => {
+  const url = typeof window !== 'undefined' ? window.location.href : '';
 
   return (
-    <div>
-      <div className="bg-transparent flex gap-2 justify-center p-2">
-        <FacebookShareButton url={imageUrlWithCurrentUrl} quote={imageUrlWithCurrentUrl} hashtag="#drtrailer">
-          <FacebookIcon size={48} round />
-        </FacebookShareButton>
+    <div className="bg-transparent flex gap-2 justify-center p-2">
+      <FacebookShareButton url={url} quote={title} hashtag="#drtrailer">
+        <FacebookIcon size={48} round />
+      </FacebookShareButton>
 
-        <TwitterShareButton url={imageUrlWithCurrentUrl} title={imageUrlWithCurrentUrl}>
-          <TwitterIcon size={48} round />
-        </TwitterShareButton>
+      <TwitterShareButton url={url} title={title}>
+        <TwitterIcon size={48} round />
+      </TwitterShareButton>
 
-        <LinkedinShareButton url={imageUrlWithCurrentUrl}>
-          <LinkedinIcon size={48} round />
-        </LinkedinShareButton>
+      <LinkedinShareButton url={url}>
+        <LinkedinIcon size={48} round />
+      </LinkedinShareButton>
 
-        <WhatsappShareButton url={imageUrlWithCurrentUrl} title={imageUrlWithCurrentUrl}>
-          <WhatsappIcon size={48} round />
-        </WhatsappShareButton>
+      <WhatsappShareButton url={url} title={title}>
+        <WhatsappIcon size={48} round />
+      </WhatsappShareButton>
 
-        <EmailShareButton url={imageUrlWithCurrentUrl} subject={imageUrlWithCurrentUrl} body="Check this out!">
-          <EmailIcon size={48} round />
-        </EmailShareButton>
-      </div>
-      <div className="text-center">
-        <p className="text-gray-500 text-sm">URL: {currentUrl}</p>
-      </div>
-      {image && (
-        <div className="text-center">
-          <p className="text-gray-500 text-sm">Image: {image}</p>
-        </div>
-      )}
+      <EmailShareButton url={url} subject={title} body="Check this out!">
+        <EmailIcon size={48} round />
+      </EmailShareButton>
     </div>
   );
 };
