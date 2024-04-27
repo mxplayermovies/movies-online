54e1d82444df6db04197f9e84be2f2327ba438f7
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-04-27 22:08:27 +0530

    update lcp

--- pages/index.js
+++ pages/index.js
@@ -286,11 +286,11 @@ const HomePage = () => {
         <ShareButtons  />
         <main className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
         <h1
-            className='flex flex-col text-center py-5 font-bold text-3xl items-center justify-center'
+            className=' text-center py-5 font-bold text-3xl items-center justify-center'
             style={{ color: '#40D7BC',  textShadow: '5px 5px 2px #000' }}
           >
             Uwatchfree Online™ - The Best Movies Platform HD Movies.  </h1>
-         <p className='flex flex-col text-center py-5 font-bold text-xl items-center justify-center'
+         <p className=' text-center font-bold text-xl items-center justify-center'
             style={{ color: '#40D7BC',  textShadow: '5px 5px 2px #000' }}
           > 
 "Uwatchfree Online™ - The Best Movies Platform HD Movies" is a platform that provides access to a wide range of high-definition movies for streaming online. With this platform, users can enjoy a diverse selection of movies spanning various genres, including action, drama, comedy, thriller, romance, and more.
