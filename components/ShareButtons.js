260dbf9a976ab4e3640a88f344f1c1d28742aba2
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-04-30 23:11:05 +0530

    update now

--- components/ShareButtons.js
+++ components/ShareButtons.js
@@ -43,6 +43,51 @@
 // export default ShareButtons;
 
 
+// import React from 'react';
+// import {
+//   FacebookShareButton,
+//   LinkedinShareButton,
+//   TwitterShareButton,
+//   WhatsappShareButton,
+//   EmailShareButton,
+//   FacebookIcon,
+//   LinkedinIcon,
+//   TwitterIcon,
+//   WhatsappIcon,
+//   EmailIcon,
+// } from 'react-share';
+
+// const ShareButtons = ({ title, description, image }) => {
+//   const url = typeof window !== 'undefined' ? window.location.href : '';
+
+//   return (
+//     <div className="bg-transparent flex gap-2 justify-center p-2">
+//       <FacebookShareButton url={url} quote={description || title} hashtag="#drtrailer">
+//         <FacebookIcon size={48} round />
+//       </FacebookShareButton>
+
+//       <TwitterShareButton url={url} title={description || title}>
+//         <TwitterIcon size={48} round />
+//       </TwitterShareButton>
+
+//       <LinkedinShareButton url={url} title={description || title}>
+//         <LinkedinIcon size={48} round />
+//       </LinkedinShareButton>
+
+//       <WhatsappShareButton url={url} title={description || title}>
+//         <WhatsappIcon size={48} round />
+//       </WhatsappShareButton>
+
+//       <EmailShareButton url={url} subject={title} body={description || "Check this out!"}>
+//         <EmailIcon size={48} round />
+//       </EmailShareButton>
+//     </div>
+//   );
+// };
+
+// export default ShareButtons;
+
+
 import React from 'react';
 import {
   FacebookShareButton,
@@ -59,26 +104,27 @@ import {
 
 const ShareButtons = ({ title, description, image }) => {
   const url = typeof window !== 'undefined' ? window.location.href : '';
+  const shareMessage = 'Watch Now !!! Uwatchfree Online™ - For Movies, TV Show & Sports!';
 
   return (
     <div className="bg-transparent flex gap-2 justify-center p-2">
-      <FacebookShareButton url={url} quote={description || title} hashtag="#drtrailer">
+      <FacebookShareButton url={url} quote={`${description || title}\n${shareMessage}`} hashtag="#drtrailer">
         <FacebookIcon size={48} round />
       </FacebookShareButton>
 
-      <TwitterShareButton url={url} title={description || title}>
+      <TwitterShareButton url={url} title={`${description || title}\n${shareMessage}`}>
         <TwitterIcon size={48} round />
       </TwitterShareButton>
 
-      <LinkedinShareButton url={url} title={description || title}>
+      <LinkedinShareButton url={url} title={`${description || title}\n${shareMessage}`}>
         <LinkedinIcon size={48} round />
       </LinkedinShareButton>
 
-      <WhatsappShareButton url={url} title={description || title}>
+      <WhatsappShareButton url={url} title={`${description || title}\n${shareMessage}`}>
         <WhatsappIcon size={48} round />
       </WhatsappShareButton>
 
-      <EmailShareButton url={url} subject={title} body={description || "Check this out!"}>
+      <EmailShareButton url={url} subject={title} body={`${description || "Check this out!"}\n\n${shareMessage}`}>
         <EmailIcon size={48} round />
       </EmailShareButton>
     </div>
