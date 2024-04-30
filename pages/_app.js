1f7310a897ade9c2575df428c150a25aa7faba9a
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-04-30 22:53:57 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ function Application({ Component, pageProps }) {
         data-admpid="82683"
         strategy="afterInteractive"
       /> */}
-        <GoogleAnalytics measurementId="G-NRD78VK7WV" />
+        <GoogleAnalytics measurementId="G-3DHHHPZ1LV" />
         {/* <Script async data-id="101405628" src="//static.getclicky.com/js"></Script> */}
      
         <PageTransition>  
