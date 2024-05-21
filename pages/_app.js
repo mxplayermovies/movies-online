860f04db59f48371aebe4017c6c79f865d15ee67
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-22 01:20:04 +0530

    update now

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Hamburger from '../components/Hamburger';
 import { PageTransition } from "../components/PageTransition";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 import Script from 'next/script';
+import TawkTo from '../components/TawkTo';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -26,6 +27,7 @@ function Application({ Component, pageProps }) {
      
         <PageTransition>  
           <Hamburger />
+          <TawkTo />
          <Component {...pageProps} />
           <Footer />
         </PageTransition>
