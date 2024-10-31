990d53feec8a6a95b20a575bc76550f9821585cc
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-10-31 23:16:25 +0530

    ok ads updated

--- public/propler/ads.js
+++ public/propler/ads.js
@@ -37,7 +37,7 @@
       // Function to load the ad code
 function loadAdCode() {
   var adScript = document.createElement('script');
-  adScript.src = '//thubanoa.com/1?z=8444298';
+  adScript.src = '//thubanoa.com/1?z=8447529';
   adScript.async = true;
   adScript.setAttribute('data-cfasync', 'false');
 
@@ -52,3 +52,4 @@ function loadAdCode() {
 
 // Set a timeout to load the ad code after 10 seconds
 setTimeout(loadAdCode, 5000); // 10000 milliseconds = 10 seconds
+{/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=8447529"></script> */}
